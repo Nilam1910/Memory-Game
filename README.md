ffed15ad6b1cb05680cd179197500c86b3924787
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-07-14 01:26:54 -0500

    Basic ready for all HTML CSS and JS

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 3-Card-Game
-find the describe object out of cards. If you win gain the point
+
+Project - 3 Card Game
+Created by Nilam Patel - July 12, 2022
+
+General Assembly, SEI524 Course
+
+User Stories
---
+++ js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+   // make an array with card and image 
+const cardsArray = [
+      
+      {card: "king",
+      image: "./images/card_Kwebp"
+      },
+      {card: "king",
+      image: "./images/card_Kwebp"
+      },
+      {card: "queen",
+      image: "./images/card_Qwebp"
+      },
+      {card: "queen",
+      image: "./images/card_Qwebp"
+      }, 
+      {card: "A",
+      image: "./images/card_A.webp"
+      },
+      {card: "A",
+      image: "./images/card_A.webp"
+      }, 
+      {card: "Joker",
+      image: ".images/card_J.webp"
+      }, 
+      {card: "Joker",
+      image: ".images/card_J.webp"
+      },
+      {card: "J",
+      image: ".images/card_J1.webp"
+      }, 
+      {card: "J",
+      image: ".images/card_J1.webp"
+      }   
+   ]
+})
