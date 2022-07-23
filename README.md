98207ae4124741b9d32954310a8a0e340871c22e
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-07-23 00:10:48 -0500

    Allmost Done

--- README.md
+++ README.md
@@ -1,8 +1,59 @@
-# 3-Card-Game
+      Memory Game
 
-Project - 3 Card Game
+Project - Memory Game
 Created by Nilam Patel - July 12, 2022
 
 General Assembly, SEI524 Course
 
-User Stories
+## User Stories:
+
+Two player game where users have dealt 5pair cards. The cards are flipped by clicking, user need to find pair of card by memories the place of cards. Finding each pair of car user earn the point. who ever get more pair of cards get win the game.
+
+## Game flow:
+
+- Click the card to flip over
+- Click another card to flip over and find the pair of card
+- Then other user do same process
+- Both user take turn after each attemps
+- Every time who evern found the correct pair of the cards earn point
+- Repet the process untill you find all pair of card
+- who ever found more pairs of card wins the game
+- By clicking reset button you can play again the game
+
+## Inital Wireframe:
+
+## Psedocode:
+
+- Put cards on the bord in random order
+- Click the target card
+- Toggle the cards
+- Flip the cards
+- Set an attemps and found for both players
+- compair two card match
+- set alert for found pair
+- set if not found the match
+- Set an increase number with each attemps and found card pair
+- Compair plyers found card score
+- Declair wineer by seting alert
+- Create reset button to play again the game
+
+## Technologyes Used:
+
+- HTML
+- CSS3
+- JavaScript
+- Balasamiq Wireframe
+
+## Possible Futher development:...
+
+- I would like to set the time how long take to found all the cards
+- I would like to add some music with on and off buttons
+- i would like to add 3D effects on cards
+- I would like to cteate more levels of the game
+
+## References:
+
+- www.stackoverflow.com
+
+* www.w3schools.com
+* www.elsy.com
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablity to write css before you write app.js or you can say web browser trigger HTML element more beter way to faster acess  
+document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablity to write css before you write app.js or you can say web browser trigger HTML element more beter way to faster access  
       // make an array with card and image 
       const getArray = ()=>[   
       {cardName: "A", imageSrc: "./images/card_A.webp"},
@@ -21,9 +21,9 @@ document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablit
       const grabPlayerOneFoundHolder = document.querySelector("#playerOneFoundHolder")
       const grabPlayerTwoAttemptsHolder = document.querySelector("#playerTwoAttemptsHolder")
       const grabPlayerTwoFoundHolder = document.querySelector("#playerTwoFoundHolder")
-
-      var chosenCardId =[] // we donot know which image id going to be chosen so lefted empty
-      var chosenCard = []  // we donot know which image going to be chosen so lefted empty
+      // const grabTimeHolder = document.querySelector("#timeHolder")
+      var chosenCardId =[] 
+      var chosenCard = []  
       const cardsInGame = 5   // total 5 pairs of image  
       /* var attempts = 0  // to start for chose image from 0
        var found = 0 // to start how many images found from 0
@@ -32,10 +32,12 @@ document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablit
       playerOneTurn = true
       var playerOne = {attempts: 0, found:  0} //{}because they are object
       var playerTwo = {attempts: 0, found:  0}
+      
       grabPlayerOneAttemptsHolder.textContent = playerOne.attempts
       grabPlayerOneFoundHolder.textContent = playerOne.found
       grabPlayerTwoAttemptsHolder.textContent = playerTwo.attempts
       grabPlayerTwoFoundHolder.textContent = playerTwo.found
+      
          // use sort method to randamize array
       const randamize = () => {
          const cardArray = getArray()
@@ -48,9 +50,7 @@ document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablit
          //  card generator function
          const cardGenerator = () => {
          const cardArray = randamize()
-         console.log(cardArray)
-         // cardArray.forEach((item)=> // not needed after made chages
-         
+         // console.log(cardArray)
          function board (){
             for(let i=0; i<cardArray.length; i++){
                console.log(i)
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablit
                const back = document.createElement("img")
                const front = document.createElement("img")
                back.setAttribute("name", cardArray[i].cardName )
-               card.setAttribute("name", cardArray[i].cardName + "div") //geting back when you do surch card that why need to set up this way
+               card.setAttribute("name", cardArray[i].cardName + "div") //geting back when you do surch card that why need to set up this 
                card.classList = "card" // made class of each
                front.classList = "front"
                back.classList = "back"
@@ -67,9 +67,7 @@ document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablit
                card.appendChild(front)
                back.src = cardArray[i].imageSrc // back image attached it
                // grab another image for font and attached with 
-               front.setAttribute("src","images/Rainbow_background.webp")// in css you have to write ( .card{position: relative; transform-style: prserve-3d;transsition: all 2s ease; transform: rotateY(180deg) } and .face, .front{ position: absolute;  backface-visibility: hidden}) to set image top of back image and
-            
-               //   front.addEventListener("click", "Flip")  // not needed after made chages 
+               front.setAttribute("src","images/Rainbow_background.webp") 
                card.addEventListener("click", (e) =>{ //event been triger with (e)
                   card.classList.toggle("toggleCard") //use for animation
                   card.classList.add("flipCard") // easy way to ask browse to slect the card what we going to compair
@@ -81,85 +79,100 @@ document.addEventListener("DOMContentLoaded", ()=>{    // it will give you ablit
          board()
    }      
       const checkCards = (e) => {
-            //  console.log(e) // worked
-            chosenCard = document.querySelectorAll(".flipCard")
-              //chosencard from ref line 22 
-            //  console.log(chosenCard)  //worked in emapty array grab all [div.card.toggleCard.flipCard]
+               //  console.log(e) // worked
+               chosenCard = document.querySelectorAll(".flipCard") //chosencard empty array from ref line 22 
+               //  console.log(chosenCard)  //worked in empty array grab all [div.card.toggleCard.flipCard]
             if(chosenCard.length===2){
                   // console.log(chosenCard[1]) //when we click second card then we grab name and src both
                   checkForMatch()
-                  // grabFoundHolder.textContent = found
-                  // grabAttemptsHolder.textContent = attempts 
+                  /* grabFoundHolder.textContent = found
+                     grabAttemptsHolder.textContent = attempts */ // for only player game
                   grabPlayerOneAttemptsHolder.textContent = playerOne.attempts
                   grabPlayerOneFoundHolder.textContent = playerOne.found
                   grabPlayerTwoAttemptsHolder.textContent = playerTwo.attempts
                   grabPlayerTwoFoundHolder.textContent = playerTwo.found 
+                  grabPlayerTwoFoundHolder.textContent = playerTwo.found
+                  
                   playerOneTurn = !playerOneTurn // that gives playerOne and plyerTwo turn by turn
                }
          }      
          function checkForMatch(){
-               // attempts++; //do if state    will increase after every two card chosen. see line 85
-                  if (playerOneTurn == true){
+               // attempts++; //will increase after every two card chosen. see line 85    // for only player game
+               if(playerOneTurn == true){
                      playerOne.attempts++
                   }else{
                      playerTwo.attempts++
                   }
-               if(chosenCard[0].getAttribute("name") === chosenCard[1].getAttribute("name")) { //  store infomation in div under the name attribute
+               if(chosenCard[0].getAttribute("name") === chosenCard[1].getAttribute("name")) { //store infomation in div under the name attribute
                      if(playerOneTurn == true){
                         playerOne.found++
+                        // time --
                      }else{
                         playerTwo.found++
                      }
-                  // found++// do if state
-                  alert("Good job! you found the match")
-                  chosenCard[0].classList.remove("flipCard" )
-                  chosenCard[1].classList.remove("flipCard" )
-                  // chosenCard[0].setAttribute("src", "images/card_blank.png")//accessing 
-                  // chosenCard[1].setAttribute("src", "images/card_blank.png") 
-                  
-                  // if(found == cardsInGame){
-                  //    alert("Nice job you found all the cards ")
-                  // }
-                  
+                     // found++      // for only player game 
+                     alert("Good job! you found the match")
+                     chosenCard[0].classList.remove("flipCard")
+                     chosenCard[1].classList.remove("flipCard")
+                     /* if(found == cardsInGame){
+                         alert("Nice job you found all the cards ")
+                        }  */    // for only player game        
                   if (playerOne.found + playerTwo.found === cardsInGame) {
-                        // game is finished
+                        
+                        alert("Game is Finished")
+                        
                         if (playerOne.found>playerTwo.found) {
+                           
                            alert("Player One Wins The Game")
-                        //playerOne wins
-                        } else {
+                        
+                        }else{
+                           
                            alert("Player Two Wins The Game")
-                        // playerTwo wins
-                      }
+                           
+                        }
                      }
-                     
+
+                   }else{
+                     alert("keep trying to find the match")
              
-               }else{
-                  alert("keep trying to find the match")
                   setTimeout(()=>{
+
                      chosenCard[0].classList.toggle("toggleCard" )
                      chosenCard[1].classList.toggle("toggleCard" )
+
                   },1000)
                   
                   chosenCard[0].classList.remove("flipCard" )
                   chosenCard[1].classList.remove("flipCard" )
-                  // chosenCard[0]=classList //setAttribute("src","images/Rainbow_background.webp")
-                  // chosenCard[1]=classList //setAttribute("src","images/Rainbow_background.webp")
-                  
                }  
             }
             cardGenerator()
+   })
          
-         // checkCards() no need
-         // checkForMatch() no need to invock again because we did in checkcards() 
-})
    
+
+
+
+
+
+
          // to make two player game  
          // do chage html attempt and found for each player
          // make them variable
          // grab them
+         // line 97 must needed
+         // if staement for attems and found
          // add to score found in game
          // plyerone found = player found
-         // then declair wins  
+         // if statement to declair winers  
 
-
+         //for add time we can start with following
+         /*  
+         const grabTimeHolder = document.querySelector("#timeHolder")
+         var time = {time: 15}
+         grabTimeHolder.textContent = time.time
+         if(playerOne + playerTwo == time){
+                            time --
+                            alert("Game time finished")
+                         } */
    
